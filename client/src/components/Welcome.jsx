2e55b373fa07a9eb9692647badbdc431843e1087
Noel Jose <noeljose@pop-os.localdomain>
2022-06-20 07:14:46 +0530

    Created a custom input component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,6 +7,17 @@ import { Loader } from './';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+const Input = ({placeholder,name,type,value,handleChange}) => (
+  <input 
+    placeholder={placeholder} 
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e,name)}
+    className="my-2 w-full rounded-sm p-2 outine-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+    
+)
 
 const  Welcome = () => {
 
@@ -62,7 +73,7 @@ const  Welcome = () => {
         </div>
 
         <div className='p-5  sm:w-96 w-full flex flex-col justify-center items-center blue-glassmorphism'>
-
+          <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}} />
         </div>
       </div>
 
