3558f0cb84edb2034db126a599d71a1d713c48c4
Noel Jose <noeljose@pop-os.localdomain>
2022-06-20 07:06:34 +0530

    Completed the Ethereum address card

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -41,16 +41,29 @@ const  Welcome = () => {
         </div>
       </div>
 
-      <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
-        <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 white-glassmorpism eth-card"></div>
-          <div className='flex justify-between flex-col w-full h-full'>
-            <div className='flex justify-between items-start'>
-              <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
-                <SiEthereum fontSize={21} color="#fff"/>
+      <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+        <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+          <div className="flex justify-between flex-col w-full h-full">
+            <div className="flex justify-between items-start">
+              <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                <SiEthereum fontSize={21} color="#fff" />
               </div>
-              <BsInfoCircle fontSize={17} color="#fff"/>
+                <BsInfoCircle fontSize={17} color="#fff" />
+            </div>
+            <div>
+              <p className='text-white font-light text-sm'>
+                Address
+              </p>
+              <p className='text-white font-semibold text-lg mt-1'>
+                Ethereum
+              </p>
             </div>
           </div>
+        </div>
+
+        <div className='p-5  sm:w-96 w-full flex flex-col justify-center items-center blue-glassmorphism'>
+
+        </div>
       </div>
 
     </div>
