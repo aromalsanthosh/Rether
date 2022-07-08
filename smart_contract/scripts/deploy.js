1168e50e1d13475dc061b8898b93d2de8e6fd992
Noel Jose <noeljose@pop-os.localdomain>
2022-07-08 07:30:17 +0530

    Modified deploy configurations

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+const main = async () => {
+  // We get the contract to deploy
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transaction = await Transactions.deploy();
+
+  await transaction.deployed();
+
+  console.log("Transactions deployed to:", transaction.address);
+};
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
