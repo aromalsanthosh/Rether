763fbb2a3c2c38ba2909f71dd278a3ad97f79d4d
Noel Jose <noeljose@pop-os.localdomain>
2022-07-10 17:27:50 +0530

    Created a custom gif fetch hook

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -4,6 +4,60 @@ import { RiH3 } from "react-icons/ri";
 import { TransactionContext } from "../context/TransactionContext";
 import { shortenAddress } from "../utils/shortenaddress";
 
+const TransactionCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyword,
+  amount,
+  url,
+}) => {
+  return (
+    <div
+      className="bg-[#181918] m-4 flex flex-1 
+  2xl:min-w-[450px]
+  2xl:max-w-[500px]
+  sm:min-w-[270px]
+  sm:max-w-[300px]
+  flex-col p-3 rounded-md hover:shadow-2xl
+  "
+    >
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className=" w-full mb-6 p-2">
+          <a
+            href={`https://ropsten.etherscan.io/address/${addressFrom}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-base">
+              From :{shortenAddress(addressFrom)}
+            </p>
+          </a>
+          <a
+            href={`https://ropsten.etherscan.io/address/${addressTo}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-base">
+              To :{shortenAddress(addressTo)}
+            </p>
+          </a>
+          <p className="text-white text-base">Amount: {amount} ETH</p>
+          {message && (
+            <>
+              <br />
+              <p className="text-white text-base">Message : {message}</p>
+            </>
+          )}
+          <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+            <p className="text-[#37c7da]"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 function Transactions() {
   const { currentAccount } = useContext(TransactionContext);
   return (
@@ -18,6 +72,7 @@ function Transactions() {
             Connect Your Account to see the latest transactions
           </h3>
         )}
+        <div className="flex flex-wrap justify-center items-center mt-10"></div>
       </div>
     </div>
   );
