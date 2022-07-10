1c4adf966129638348429c89e67e9baf647a6731
Noel Jose <noeljose@pop-os.localdomain>
2022-07-10 17:42:22 +0530

    Added the footer component

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -4,7 +4,10 @@ import { RiH3 } from "react-icons/ri";
 import { TransactionContext } from "../context/TransactionContext";
 import { shortenAddress } from "../utils/shortenaddress";
 
-const TransactionCard = ({
+import dummyData from "../utils/dummyData";
+import useFetch from "../hooks/useFetch";
+
+const TransactionsCard = ({
   addressTo,
   addressFrom,
   timestamp,
@@ -13,46 +16,52 @@ const TransactionCard = ({
   amount,
   url,
 }) => {
+  const gifUrl = useFetch({ keyword });
   return (
     <div
-      className="bg-[#181918] m-4 flex flex-1 
-  2xl:min-w-[450px]
-  2xl:max-w-[500px]
-  sm:min-w-[270px]
-  sm:max-w-[300px]
-  flex-col p-3 rounded-md hover:shadow-2xl
-  "
+      className="bg-[#181918] m-4 flex flex-1
+      2xl:min-w-[450px]
+      2xl:max-w-[500px]
+      sm:min-w-[270px]
+      sm:max-w-[300px]
+      min-w-full
+      flex-col p-3 rounded-md hover:shadow-2xl"
     >
       <div className="flex flex-col items-center w-full mt-3">
-        <div className=" w-full mb-6 p-2">
+        <div className="display-flex justify-start w-full mb-6 p-2">
           <a
             href={`https://ropsten.etherscan.io/address/${addressFrom}`}
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
           >
             <p className="text-white text-base">
-              From :{shortenAddress(addressFrom)}
+              From: {shortenAddress(addressFrom)}
             </p>
           </a>
           <a
             href={`https://ropsten.etherscan.io/address/${addressTo}`}
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
           >
             <p className="text-white text-base">
-              To :{shortenAddress(addressTo)}
+              To: {shortenAddress(addressTo)}
             </p>
           </a>
           <p className="text-white text-base">Amount: {amount} ETH</p>
           {message && (
             <>
               <br />
-              <p className="text-white text-base">Message : {message}</p>
+              <p className="text-white text-base">Message: {message}</p>
             </>
           )}
-          <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-            <p className="text-[#37c7da]"></p>
-          </div>
+        </div>
+        <img
+          src={gifUrl || url}
+          alt="nature"
+          className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+        />
+        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+          <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
       </div>
     </div>
@@ -72,7 +81,11 @@ function Transactions() {
             Connect Your Account to see the latest transactions
           </h3>
         )}
-        <div className="flex flex-wrap justify-center items-center mt-10"></div>
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {[...dummyData].reverse().map((transaction, i) => (
+            <TransactionsCard key={i} {...transaction} />
+          ))}
+        </div>
       </div>
     </div>
   );
